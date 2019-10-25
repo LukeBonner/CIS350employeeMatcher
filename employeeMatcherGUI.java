f225795238316eb40ccad4bc840c442bb8230432
GrewalSim <54866232+GrewalSim@users.noreply.github.com>
2019-10-25 12:43:59 -0400

    Add files via upload

---
+++ employeeMatcherGUI.java
@@ -0,0 +1,25 @@
+package employeeMatcher;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class employeeMatcherGUI extends JFrame {
+    public employeeMatcherGUI(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.getContentPane().add(new employeeMatcherPanel());
+        this.setSize(500,500);
+        this.setPreferredSize(new Dimension(500,500));
+        this.setVisible(true);
+    }
+
+
+
+
+    public static void main(String[] args){
+        new employeeMatcherGUI("Employment Matcher!");
+    }
+
+}
+
---
+++ employeeMatcherPanel.java
@@ -0,0 +1,87 @@
+package employeeMatcher;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ */
+public class employeeMatcherPanel extends JPanel {
+    private JButton acceptButton;
+    private JButton declineButton;
+    private JButton extremeInterestButton;
+    private JButton slideNextImage;
+    private JButton slideLastImage;
+    private JLabel emptyArea;
+
+
+    /**
+     *
+     */
+    public employeeMatcherPanel(){
+        JPanel displayArea = new JPanel();
+        JPanel buttonArea = new JPanel();
+
+
+
+        buttonListener theListener = new buttonListener();
+
+
+        this.acceptButton = new JButton("interested");
+        this.declineButton = new JButton("not interested");
+        this.extremeInterestButton = new JButton("very interested");
+        this.slideLastImage = new JButton("<--");
+        this.slideNextImage = new JButton("-->");
+        this.emptyArea = new JLabel();
+        displayArea.setPreferredSize(new Dimension(200,200));
+        buttonArea.setLayout(new GridLayout(2,3));
+        displayArea.setLayout(new GridLayout(1,1));
+
+
+        buttonArea.add(slideLastImage);
+        buttonArea.add(emptyArea);
+        buttonArea.add(slideNextImage);
+        buttonArea.add(declineButton);
+        buttonArea.add(extremeInterestButton);
+        buttonArea.add(acceptButton);
+
+
+        slideNextImage.addActionListener(theListener);
+        declineButton.addActionListener(theListener);
+        extremeInterestButton.addActionListener(theListener);
+        acceptButton.addActionListener(theListener);
+        slideLastImage.addActionListener(theListener);
+
+
+
+
+
+        this.add(displayArea, BorderLayout.CENTER);
+        this.add(buttonArea, BorderLayout.SOUTH);
+
+    }
+
+    private class buttonListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource()==slideLastImage){
+
+            }else if(e.getSource() == slideNextImage){
+
+            }else if(e.getSource()== declineButton){
+
+            }else if(e.getSource()==extremeInterestButton){
+
+            }else if(e.getSource()==acceptButton){
+
+            }
+        }
+    }
+
+
+
+}
