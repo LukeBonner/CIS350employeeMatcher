8682e3e7b92effbb07b5eb3210bbf2ce37414eb1
Luke Bonner <skier1337@gmail.com>
2019-12-04 11:39:32 -0500

    updated GUI space

---
+++ src/employeematcher/EmployeeMatcherGUI.java
@@ -0,0 +1,44 @@
+package employeeMatcher;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.security.Key;
+import javax.swing.*;
+
+
+public class EmployeeMatcherGUI extends JFrame {
+
+  public EmployeeMatcherUI(String title) {
+    super(title);
+    int user = JOptionPane.showInputDialog("Are you ")
+    JTabbedPane tabs = new JTabbedPane();
+    EmployeeMatcherPanel matcherPanel = new EmployeeMatcherPanel();
+    matcherAccountPanel accountPanel = new matcherAccountPanel();
+
+    tabs.add("Matcher",matcherPanel);
+    tabs.setMnemonicAt(0, KeyEvent.VK_1);
+
+    tabs.add("Account", accountPanel);
+    tabs.setMnemonicAt(1, KeyEvent.VK_2);
+
+    this.add(tabs,BorderLayout.CENTER);
+
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    this.setSize(500,500);
+    this.setPreferredSize(new Dimension(500,500));
+    this.pack();
+    this.setVisible(true);
+  }
+
+
+
+  public static void main(String[] args) {
+
+    new EmployeeMatcherUI("Employment Matcher!");
+  }
+
+}
+
