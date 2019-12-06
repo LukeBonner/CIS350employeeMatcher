a40fb37c6b717c6e51e628bba4c6b7bf77599bf5
danno3321 <45361485+danno3321@users.noreply.github.com>
2019-12-05 20:19:59 -0500

    Add files via upload

---
+++ src/EmployeeMatcher/MatcherSeeker.java
@@ -0,0 +1,233 @@
+package EmployeeMatcher;
+
+import java.util.ArrayList;
+
+/**
+ * Holds the information for a Seeker users and provides access for other functions.
+ *
+ * This class holds the information for the Seeker type of MatcherUser,
+ * providing functions to both modify and access these values in other
+ * points of the program. Additionally, the class generates a String used
+ * by DataReadWriter to save and recover information for
+ */
+public class MatcherSeeker extends EmployeeMatcherUser {
+
+  private String firstName;
+
+  private String industry;
+
+  private String lastName;
+
+  private String personalStatement;
+
+  private ArrayList<String> education;
+
+  private ArrayList<String> experience;
+
+  private ArrayList<String> urls;
+
+
+  public MatcherSeeker() {
+    uname = "";
+    pword = "";
+    firstName = "EMPTY";
+    industry = "";
+    lastName = "";
+    personalStatement = "";
+    education = new ArrayList<String>();
+    experience = new ArrayList<String>();
+    images = new ArrayList<String>();
+    urls = new ArrayList<String>();
+    type = 0;
+  }
+
+  public MatcherSeeker(String uname, String pword, String firstName, String industry, String lastName,
+                       String personalStatement, ArrayList<String> education,
+                       ArrayList<String> experience,
+                       ArrayList<String> paths, ArrayList<String> urls, ArrayList<String> matches) {
+    this.uname = uname;
+    this.pword = pword;
+    this.firstName = firstName;
+    this.industry = industry;
+    this.lastName = lastName;
+    this.personalStatement = personalStatement;
+    this.education = education;
+    this.experience = experience;
+    this.images = paths;
+    this.matches = matches;
+    this.urls = urls;
+    type = 0;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getIndustry() {
+    return industry;
+  }
+
+  public void setIndustry(String industry) {
+    this.industry = industry;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFullName() {
+    return this.firstName + " " + this.lastName;
+  }
+  public String getPersonalStatement() {
+    return personalStatement;
+  }
+
+  public void setPersonalStatement(String personalStatement) {
+    this.personalStatement = personalStatement;
+  }
+
+  public String getEducation() {
+    String edu = "";
+    for (int i = 0; i < education.size(); i++) {
+      if (i == 0) {
+        edu = education.get(0);
+        continue;
+      }
+      edu += ", " + education.get(i);
+    }
+    return edu;
+  }
+
+  public void setEducation(ArrayList<String> education) {
+    this.education = education;
+  }
+
+  public String getExperience() {
+    String exp = "";
+    for (int i = 0; i < experience.size(); i++) {
+      if (i == 0) {
+        exp = experience.get(0);
+        continue;
+      }
+      exp += ", " + experience.get(i);
+    }
+    return exp;
+  }
+
+  public void setExperience(ArrayList<String> experience) {
+    this.experience = experience;
+  }
+
+  public void addExperience(String toAdd) {
+    this.experience.add(toAdd);
+  }
+
+  public void addEducation(String toAdd) {
+    this.education.add(toAdd);
+  }
+
+  public void addUrl(String toAdd) {
+    this.urls.add(toAdd);
+  }
+
+  public ArrayList<String> getImagesSeeker() {
+    return images;
+  }
+
+  public void addImage(String image) {
+    images.add(image);
+  }
+
+  public String getUrls() {
+    String url = "";
+    for (int i = 0; i < urls.size(); i++) {
+      if (i == 0) {
+        url = urls.get(0);
+        continue;
+      }
+      url += ", " + urls.get(i);
+    }
+    return url;
+  }
+
+  public void setUrls(ArrayList<String> urls) {
+    this.urls = urls;
+  }
+
+  public String toString() {
+    return firstName;
+  }
+
+  public String toWrite() {
+    String output = "0" + "," + uname + "," + pword + "," + firstName + "," + lastName + ",";
+    output += personalStatement + "," + industry + ",";
+    if (education.size() == 0) {
+      output += "BLANK|";
+    } else {
+      output += education.get(0);
+      for (int i = 1; i < (this.education).size(); i++) {
+        output += "," + education.get(i);
+      }
+      output += "|";
+    }
+
+    if (experience.size() == 0) {
+      output += "BLANK|";
+    } else {
+      output += experience.get(0);
+      for (int i = 1; i < (this.experience).size(); i++) {
+        output += "," + experience.get(i);
+      }
+      output += "|";
+    }
+
+    if (urls.size() == 0) {
+      output += "BLANK|";
+    } else {
+      output += urls.get(0);
+      for (int i = 1; i < (this.urls).size(); i++) {
+        output += "," + urls.get(i);
+      }
+      output += "|";
+    }
+
+    if (images.size() > 0) {
+      boolean first = true;
+      for (String path : images) {
+        if (first) {
+          output += path;
+          first = false;
+          continue;
+        }
+        output += "," + path;
+      }
+      output += "|";
+    } else {
+      output += "BLANK|";
+    }
+
+    if (matches.size() > 0) {
+      boolean first = true;
+      for (String match : matches) {
+        if (first) {
+          output += match;
+          first = false;
+          continue;
+        }
+        output += "," + match;
+      }
+      output += "|*";
+    } else {
+      output += "BLANK|*";
+    }
+    return output;
+  }
+}
---
+++ src/EmployeeMatcher/matcherAccountPanel.java
@@ -0,0 +1,73 @@
+package EmployeeMatcher;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class matcherAccountPanel extends JPanel {
+  private JPanel accountDetails;
+  private JPanel accountInfoPanel;
+  private JLabel accountDetail1;
+  private JLabel accountDetail2;
+  private JLabel accountDetail3;
+  private JLabel accountDetail4;
+  private JLabel accountDetail5;
+  private JLabel accountDetail6;
+  private JLabel accountDetail7;
+  private EmployeeMatcherUser currentUser;
+  private Hub hubInfo;
+
+
+  public matcherAccountPanel(EmployeeMatcherUser user) {
+    hubInfo = new Hub(true);
+    currentUser = user;
+    System.out.println("Current User: " + currentUser);
+
+    this.accountDetails = new JPanel();
+    this.setLayout(new GridLayout(7, 1));
+
+    this.accountInfoPanel = new JPanel();
+
+
+    this.accountDetail1 = new JLabel("");
+    this.accountDetail2 = new JLabel("");
+    this.accountDetail3 = new JLabel("");
+    this.accountDetail4 = new JLabel("");
+    this.accountDetail5 = new JLabel("");
+    this.accountDetail6 = new JLabel("");
+    this.accountDetail7 = new JLabel("");
+
+    this.accountDetails = new JPanel();
+
+    if (currentUser.getType() == 0) {
+      MatcherSeeker s = (MatcherSeeker) currentUser;
+      // show seekers
+      accountDetail1.setText("Name: " + s.getFullName());
+      accountDetail2.setText("Industry: " + s.getIndustry());
+      accountDetail3.setText("Personal Statement: " + s.getPersonalStatement());
+      accountDetail4.setText("Education: " + s.getEducation());
+      accountDetail5.setText("Experience: " + s.getExperience());
+      accountDetail6.setText("Links: " + s.getUrls());
+    } else {
+      // show employers
+      MatcherEmployer e = (MatcherEmployer) currentUser;
+
+      accountDetail1.setText("Company name: " + e.getCompanyName());
+      accountDetail2.setText("Location: " + e.getLocation());
+      accountDetail3.setText("Size: " + e.getSize());
+      accountDetail4.setText("Industry: " + e.getIndustry());
+      accountDetail5.setText("Position: " + e.getPosition());
+      accountDetail6.setText("Job Description: " + e.getJobDescription());
+      accountDetail7.setText("About us: " + e.getCompanyDescription());
+    }
+
+    this.add(accountDetail1);
+    this.add(accountDetail2);
+    this.add(accountDetail3);
+    this.add(accountDetail4);
+    this.add(accountDetail5);
+    this.add(accountDetail6);
+    this.add(accountDetail7);
+
+
+  }
+}
